7d4d2a0c154ff0af255867fd84a87e2f271180a7
Dmitree <color.ivanov2016@yandex.ru>
2023-03-30 20:08:29 +0300

    second commit

---
+++ src/main/java/test/task/applicationhandler/ApplicationHandlerApplication.java
@@ -0,0 +1,13 @@
+package test.task.applicationhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationHandlerApplication.class, args);
+	}
+
+}
