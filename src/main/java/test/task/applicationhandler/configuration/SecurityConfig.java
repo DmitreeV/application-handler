67c2197491eb3d19b86e2b05dda55569d73687ce
Dmitree <color.ivanov2016@yandex.ru>
2023-07-09 22:45:56 +0300

    add BCryptPasswordEncoder

--- src/main/java/test/task/applicationhandler/configuration/SecurityConfig.java
+++ src/main/java/test/task/applicationhandler/configuration/SecurityConfig.java
@@ -1,34 +1,36 @@
 package test.task.applicationhandler.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(12);
     }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user")
-                .password("user")
+                .password(passwordEncoder().encode("user"))
                 .roles("USER")
                 .and()
                 .withUser("admin")
-                .password("admin")
+                .password(passwordEncoder().encode("admin"))
                 .roles("ADMIN")
                 .and()
                 .withUser("operator")
-                .password("operator")
+                .password(passwordEncoder().encode("operator"))
                 .roles("OPERATOR");
     }
 
